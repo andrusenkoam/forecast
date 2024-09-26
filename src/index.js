ff7dc6d30369aac541ef0a36b15fbd5ada39ef7c
andriy andrusenko <andrusenkone@gmail.com>
2024-09-26 15:45:22 +0300

    third commit

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ function onSearchClick(evt) {
 
   const { query, days } = evt.currentTarget.elements;
 
-  getWeather(query.value, days.value).then(
-    data => (listEl.innerHTML = createMarkup(data.forecast.forecastday))
-  );
+  getWeather(query.value, days.value)
+    .then(data => (listEl.innerHTML = createMarkup(data.forecast.forecastday)))
+    .catch(err => console.log(err));
 }
 
 function getWeather(city, days) {
-  const BASR_URL = 'http://api.weatherapi.com/v1';
+  const BASE_URL = 'https://api.weatherapi.com/v1';
   const API_KEY = 'a9b2b5b585064de1aab74606242609';
 
   return fetch(
-    `${BASR_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
+    `${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
   ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
