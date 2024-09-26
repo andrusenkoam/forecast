b06d83d94dd09ce68457d33032e23a0425effb2f
andriy andrusenko <andrusenkone@gmail.com>
2024-09-26 15:39:30 +0300

    adds uk language

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ function onSearchClick(evt) {
 
   const { query, days } = evt.currentTarget.elements;
 
-  getWeather(query.value, days.value)
-    .then(data => (listEl.innerHTML = createMarkup(data.forecast.forecastday)))
-    .catch(err => console.log(err));
+  getWeather(query.value, days.value).then(
+    data => (listEl.innerHTML = createMarkup(data.forecast.forecastday))
+  );
 }
 
 function getWeather(city, days) {
@@ -18,7 +18,7 @@ function getWeather(city, days) {
   const API_KEY = 'a9b2b5b585064de1aab74606242609';
 
   return fetch(
-    `${BASR_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}`
+    `${BASR_URL}/forecast.json?key=${API_KEY}&q=${city}&days=${days}&lang=uk`
   ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
@@ -41,7 +41,7 @@ function createMarkup(arr) {
         <img src="${icon}" alt="${text}" />
         <p class="text">${text}</p>
         <h2>${date}</h2>
-        <h3>${avgtemp_c} C</h3>
+        <h3>${avgtemp_c} C&ordm;</h3>
       </li>`
     )
     .join('');
